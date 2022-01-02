a6687792fcb1b7c706977022b935e47fd728f18d
Joe Reid <joe@joereid.com>
2022-01-02 18:59:12 -0500

    defined the flag constants

---
+++ q2dmflags.go
@@ -0,0 +1,29 @@
+package main
+
+const (
+	NoHealth        = 1
+	NoItems         = 2
+	WeaponsStay     = 4
+	NoFallingDamage = 8
+	InstantItems    = 16
+	SameLevel       = 32
+	SkinTeams       = 64
+	ModelTeams      = 128
+	NoFriendlyFire  = 256
+	SpawnFarthest   = 512
+	ForceRespawn    = 1024
+	NoArmor         = 2048
+	AllowExit       = 4096
+	InfiniteAmmo    = 8192
+	QuadDrop        = 16384
+	FixedFOV        = 32768
+	QuadFireDrop    = 65536
+	NoMines         = 131072
+	NoStackDouble   = 262144
+	NoNukes         = 5624288
+	NoSpheres       = 1048576
+)
+
+func main() {
+
+}
